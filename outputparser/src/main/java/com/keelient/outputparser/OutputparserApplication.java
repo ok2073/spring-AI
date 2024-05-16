522f3bc82adda38f8d1f91ee0f1a6a6fe80c7712
ok2073 <ok2073@proton.me>
2024-05-16 20:41:18 +0200

    add outputparser

---
+++ outputparser/src/main/java/com/keelient/outputparser/OutputparserApplication.java
@@ -0,0 +1,13 @@
+package com.keelient.outputparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutputparserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutputparserApplication.class, args);
+	}
+
+}
---
+++ outputparser/src/test/java/com/keelient/outputparser/OutputparserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keelient.outputparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutputparserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
