84f46deda7ca620efafe7708124c286686da3094
ok2073 <ok2073@proton.me>
2024-05-17 11:31:53 +0200

    stuff commit

--- outputparser/src/main/java/com/keelient/outputparser/controller/SongsController.java
+++ outputparser/src/main/java/com/keelient/outputparser/controller/SongsController.java
@@ -1,15 +1,49 @@
 package com.keelient.outputparser.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.parser.ListOutputParser;
+import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api/")
+import java.util.List;
+import java.util.Map;
 
+@RestController
+@RequestMapping("/api/songs")
+@AllArgsConstructor
 public class SongsController {
 
     private final ChatClient chatClient;
 
+    @GetMapping
+    public String getSongsByArtist(@RequestParam(value = "artist", defaultValue = "Taylor Swift") String artist) {
+        var message = """
+                Please give me a list of top 10 songs for the artist {artist}.  If you don't know the answer , just say "I don't know".
+                """;
+        PromptTemplate promptTemplate = new PromptTemplate(message, Map.of("artist", artist));
+        Prompt prompt = promptTemplate.create();
+        ChatResponse response = chatClient.call(prompt);
+       return response.getResult().getOutput().getContent();
+}
 
+    @GetMapping("/outputParser")
+    public List<String> getSongsByArtistOp(@RequestParam(value = "artist", defaultValue = "Taylor Swift") String artist) {
+        var message = """
+                Please give me a list of top 10 songs for the artist {artist}.  If you don't know the answer , just say "I don't know".
+                {format}
+                """;
+        ListOutputParser listOutputParser = new ListOutputParser(new DefaultConversionService());
+
+        PromptTemplate promptTemplate = new PromptTemplate(message, Map.of("artist", artist,"format", listOutputParser.getFormat()));
+        Prompt prompt = promptTemplate.create();
+        ChatResponse response = chatClient.call(prompt);
+        return listOutputParser.parse(response.getResult().getOutput().getContent());
+    }
 }
---
+++ outputparser/src/main/java/com/keelient/outputparser/domain/Author.java
@@ -0,0 +1,6 @@
+package com.keelient.outputparser.domain;
+
+import java.util.List;
+
+public record Author(String author, List<String> books) {
+}
