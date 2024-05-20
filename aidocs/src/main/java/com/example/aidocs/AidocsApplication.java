253a90477c097515f0ec6bcf5685e79038978cb5
ok2073 <ok2073@proton.me>
2024-05-20 21:29:14 +0200

    add functions

--- aidocs/src/main/java/com/example/aidocs/AidocsApplication.java
+++ aidocs/src/main/java/com/example/aidocs/AidocsApplication.java
@@ -2,7 +2,9 @@ package com.example.aidocs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.command.annotation.CommandScan;
 
+@CommandScan
 @SpringBootApplication
 public class AidocsApplication {
 
